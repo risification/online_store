8cab93410fdc33f513a37dc4a8a0009a802f797a
risification <sultangaziev01@bk.ru>
2021-01-29 15:53:48 +0600

    initialize project

--- products/admin.py
+++ products/admin.py
@@ -5,7 +5,10 @@ from .models import Products, Order, Aboutus,Cotancts
 # Register your models here.
 class Product_top(admin.ModelAdmin):
     list_display = ['image', 'name', 'description', 'type', 'price']
-
+class Contacts_top(admin.ModelAdmin):
+    list_display = ['name_user','last_name_user','phone','email','address','longtitude','latitude']
 
 admin.site.register(Products, Product_top)
-admin.site.register([Order, Aboutus,Cotancts])
+admin.site.register([Order, Aboutus])
+admin.site.register(Cotancts, Contacts_top)
+
