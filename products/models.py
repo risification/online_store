9c4606f92a14aab8c9425449b20f1a2211da6a63
risification <sultangaziev01@bk.ru>
2021-01-29 15:43:25 +0600

    initialize project

--- products/models.py
+++ products/models.py
@@ -37,10 +37,10 @@ class Aboutus(models.Model):
 
 
 class Cotancts(models.Model):
-    name_user = models.CharField(max_length=20,blank=True,null=True)
-    last_name_user = models.CharField(max_length=20,blank=True,null=True)
+    name_user = models.CharField(max_length=20)
+    last_name_user = models.CharField(max_length=20)
     phone = models.CharField(max_length=10)
     email = models.EmailField()
-    address = models.CharField(max_length=50,blank=True,null=True)
-    lon = models.FloatField()
-    lat = models.FloatField()
+    address = models.CharField(max_length=50)
+    longtitude = models.IntegerField(blank=True,null=True)
+    latitude = models.IntegerField(blank=True,null=True)
